10c05202de08e62e9ce8b368194a2b5995a12774
arielramos03 <103594586+ramoscariel@users.noreply.github.com>
2025-07-15 22:05:38 -0500

    update SecurityConfig

--- src/main/java/com/b/back/SecurityConfig.java
+++ src/main/java/com/b/back/SecurityConfig.java
@@ -29,13 +29,13 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5174"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization","Content-Type"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/**",configuration);
+        source.registerCorsConfiguration("/proxy/**",configuration);
         return source;
     }
 
